8e920cf17b427fcac53e459dd95598c84c8c3f51
Roger Pence <roger.pence@gmail.com>
2014-10-06 15:35:20 -0500

    Change autocomplete js to new version

--- public/js/ASNAHelper.AutoComplete.js
+++ public/js/ASNAHelper.AutoComplete.js
@@ -1,49 +1,80 @@
-﻿var ASNAHelper = ASNAHelper || {};
+﻿(function (globals){
 
+var ASNAHelper = globals.ASNAHelper = (globals.ASNAHelper || {});
 
-ASNAHelper.AutoComplete = function () {
+function getQueryFieldValue(valueSpecified){
+        // If the value of a query field starts with a # is assumed 
+        // to be a selector and the value of that select should be used. 
+        // This code looks first to the val() property of form elements
+        // first, and then to the text() property. If an element isn't 
+        // found with either search, the original value specified
+        // is returned. 
+        if (typeof valueSpecified == 'undefined') return 'undefined';
+        if (valueSpecified.substring(0,1) === "#") {
+            var selector = $(valueSpecified);
+            if (selector.length > 0) {
+                var value = selector.val();
+                if (value) return value;
+                if (!value) {
+                    value = selector.text();
+                    if (value) return value;
+                }
+            }
+            return valueSpecified;
+        }
+        else {
+            return valueSpecified;
+        }
+    }
+
+var AutoComplete = function(args) {
     this.url = null;
     this.ownerId = null;
     this.valueTargetId = null;
     this.labelTargetId = null;
     this.showLabelOnScroll = null;
+    this.onSelect = args.onSelect;
 }
 
-ASNAHelper.AutoComplete.protoype = {
+AutoComplete.prototype = {
     source: 
     function(req,add,ajaxArgs,configArgs) {
-                var url = configArgs.url;
-                this.ownerId = configArgs.ownerId;
-                if (configArgs.hasOwnProperty("valueTargetId")) this.valueTargetId = configArgs.valueTargetId;
-                if (configArgs.hasOwnProperty("labelTargetId")) this.labelTargetId = configArgs.labelTargetId;
-                if (configArgs.hasOwnProperty("showLabelOnScroll")) this.showLabelOnScroll = configArgs.showLabelOnScroll;
+        var url = configArgs.url;
+        this.ownerId = configArgs.ownerId;
+        if (configArgs.hasOwnProperty("valueTargetId")) this.valueTargetId = configArgs.valueTargetId;
+        if (configArgs.hasOwnProperty("labelTargetId")) this.labelTargetId = configArgs.labelTargetId;
+        if (configArgs.hasOwnProperty("showLabelOnScroll")) this.showLabelOnScroll = configArgs.showLabelOnScroll;
 
-                $(".my-ui-icon-alert").removeClass("my-ui-icon-alert");
+        if (ajaxArgs.hasOwnProperty("qryval1")) ajaxArgs.qryval1 = getQueryFieldValue(ajaxArgs.qryval1);
+        if (ajaxArgs.hasOwnProperty("qryval2")) ajaxArgs.qryval2 = getQueryFieldValue(ajaxArgs.qryval2);
+        if (ajaxArgs.hasOwnProperty("qryval3")) ajaxArgs.qryval3 = getQueryFieldValue(ajaxArgs.qryval3);
 
-                var queryString = $.param(ajaxArgs);
-                var fullUrl = url + "?" + queryString;
-                var promise = $.getJSON(fullUrl)
-                .done(function (data) {
-                    if (data.list) {
-                        if (data.list.length > 0) {
-                            add(data.list);
-                        } else {
-                            $(".ui-autocomplete-loading").addClass("my-ui-icon-alert");
-                        }
-                    }
-                    else if (data.error) {
-                        var x = data;
-                    }
-                })
-                .fail(function (jqXHR,textStatus,errorThrown) {
-                    console.log("error");
-                    console.log("error " + textStatus);
-                    console.log("incoming Text " + jqXHR.responseText);
-                })
-                .always(function() {
-                    $(".ui-autocomplete-loading").removeClass("ui-autocomplete-loading");
-                });
-     },
+        $(".my-ui-icon-alert").removeClass("my-ui-icon-alert");
+
+        var queryString = $.param(ajaxArgs);
+        var fullUrl = url + "?" + queryString;
+        var promise = $.getJSON(fullUrl)
+        .done(function (data) {
+            if (data.list) {
+                if (data.list.length > 0) {
+                    add(data.list);
+                } else {
+                    $(".ui-autocomplete-loading").addClass("my-ui-icon-alert");
+                }
+            }
+            else if (data.error) {
+                var x = data;
+            }
+        })
+        .fail(function (jqXHR,textStatus,errorThrown) {
+            console.log("error");
+            console.log("error " + textStatus);
+            console.log("incoming Text " + jqXHR.responseText);
+        })
+        .always(function() {
+            $(".ui-autocomplete-loading").removeClass("ui-autocomplete-loading");
+        });
+    },
     select: 
     function(e,ui) {
         var result = true;
@@ -55,6 +86,11 @@ ASNAHelper.AutoComplete.protoype = {
             $("#" + this.labelTargetId).val(ui.item.label);
             result = this.labelTargetId !== this.ownerId;
         }    
+
+        if (typeof this.onSelect == "function") {
+            this.onSelect();
+        }
+
         return result;
     },
     focus: 
@@ -66,78 +102,50 @@ ASNAHelper.AutoComplete.protoype = {
         }
         return result;
     }
-}
+};
 
+ASNAHelper.RegisterAutoComplete = function() {
+    function forField(args) {
 
+        var autocompleteHelper = new AutoComplete({onSelect: args.onSelect});
 
-//ASNAHelper.AutoComplete = function () {
+        $("#" + args.labelTargetId).autocomplete({
+             source: function (req, add) {
+                var ajaxArgs = {
+                    library: args.library,
+                    file: args.file,
+                    fieldsList: args.fieldsList,
+                    rows: args.rows,
+                    qryfld1: args.qryfld1,
+                    qryval1: req.term,
+                    qryval2: args.qryval2,
+                    qryfld2: args.qryfld2,
+                    qryval3: args.qryval3,
+                    qryfld3: args.qryfld3,
+                    query: args.query
+                };
+                var configArgs = {
+                    url: args.url,
+                    ownerId: this.element.context.id,
+                    valueTargetId: args.valueTargetId,
+                    labelTargetId: args.labelTargetId,
+                    showLabelOnScroll: args.showLabelOnScroll,
+                    onSelect: args.onSelect
+                };
+                autocompleteHelper.source(req,add,ajaxArgs,configArgs);
+            },
+            select: function (e, ui) {
+                return autocompleteHelper.select(e, ui);
+            },
+            focus: function (e, ui) {
+                return autocompleteHelper.focus(e, ui);
+            }
+        });
+    };
 
-//    var url = null;
-//    var ownerId = null;
-//    var valueTargetId = null;
-//    var labelTargetId = null;
-//    var showLabelOnScroll = null;
+    return {
+        forField: forField
+    }
+}();
 
-//    function source(req,add,ajaxArgs,configArgs) {
-//        var url = configArgs.url;
-//        this.ownerId = configArgs.ownerId;
-//        if (configArgs.hasOwnProperty("valueTargetId")) this.valueTargetId = configArgs.valueTargetId;
-//        if (configArgs.hasOwnProperty("labelTargetId")) this.labelTargetId = configArgs.labelTargetId;
-//        if (configArgs.hasOwnProperty("showLabelOnScroll")) this.showLabelOnScroll = configArgs.showLabelOnScroll;
-
-//        $(".my-ui-icon-alert").removeClass("my-ui-icon-alert");
-
-//        var queryString = $.param(ajaxArgs);
-//        var fullUrl = url + "?" + queryString;
-//        var promise = $.getJSON(fullUrl)
-//        .done(function (data) {
-//            if (data.list) {
-//                if (data.list.length > 0) {
-//                    add(data.list);
-//                } else {
-//                    $(".ui-autocomplete-loading").addClass("my-ui-icon-alert");
-//                }
-//            }
-//            else if (data.error) {
-//                var x = data;
-//            }
-//        })
-//        .fail(function (jqXHR,textStatus,errorThrown) {
-//            console.log("error");
-//            console.log("error " + textStatus);
-//            console.log("incoming Text " + jqXHR.responseText);
-//        })
-//        .always(function() {
-//            $(".ui-autocomplete-loading").removeClass("ui-autocomplete-loading");
-//        });
-//    }
-
-//    function select(e,ui) {
-//        var result = true;
-//        if (this.valueTargetId) {
-//            $("#" + this.valueTargetId).text(ui.item.value);
-//            $("#" + this.valueTargetId).val(ui.item.value);
-//        }
-//        if (this.labelTargetId) {
-//            $("#" + this.labelTargetId).val(ui.item.label);
-//            result = this.labelTargetId !== this.ownerId;
-//        }
-    
-//        return result;
-//    }
-
-//    function focus(e,ui) {
-//        var result = true;
-//        if (this.showLabelOnScroll) {
-//            $("#" + this.labelTargetId).val(ui.item.label);            
-//            result = false;
-//        }
-//        return result;
-//    }
-
-//    return  {
-//        source: source,
-//        select: select,
-//        focus: focus
-//    };
-//}();
+})(this);
--- public/js/jQueryAutoComplete.aspx.js
+++ public/js/jQueryAutoComplete.aspx.js
@@ -1,30 +1,14 @@
 ﻿$(function () {
-    $("#textboxCustomerName").autocomplete({
-        source: function (req, add) {
-            var ajaxArgs = {
-                library: "examples",
-                file: "cmastnewl2",
-                fieldsList: "cmname:label,cmcustno:value",
-                rows: 12,
-                qryfld1: "cmname",
-                qryval1: req.term,
-                query: "CMNAME >= '{CMNAME}'",
-            };
-            var configArgs = {
-                url: "/services/JsonAutoComplete.ashx",
-                ownerId: this.element.context.id,
-                valueTargetId: "textboxCustomerNumber",
-                labelTargetId: "textboxCustomerName",
-                showLabelOnScroll: true
-            };
-            ASNAHelper.AutoComplete.source(req,add,ajaxArgs,configArgs);
-        },
-        select: function (e, ui) {
-            //    $("#textboxCounty").focus();
-            return ASNAHelper.AutoComplete.select(e, ui);
-        },
-        focus: function (e, ui) {
-            return ASNAHelper.AutoComplete.focus(e, ui);
-        }
+    ASNAHelper.RegisterAutoComplete.forField({
+        labelTargetId: "textboxCustomerName",
+        valueTargetId: "textboxCustomerNumber",
+        library: "examples",
+        file: "cmastnewL2",
+        fieldsList: "cmname:label,cmcustno:value",
+        rows: 12,
+        qryfld1: "cmname",
+        query: "CMNAME >= '{CMNAME}'",
+        url: "/services/JsonAutoComplete.ashx",
+        showLabelOnScroll: true
     });
 });
